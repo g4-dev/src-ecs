80861ea5c8f07d69de4273f0f0051eead2a2a738
loic-roux <69loloro10@gmail.com>
2020-01-26 17:43:45 +0100

    makefile/ configs roles

--- www/src/Core/Entity/RolesTrait.php
+++ www/src/Core/Entity/RolesTrait.php
@@ -2,21 +2,25 @@
 
 namespace Core\Entity;
 
-use Doctrine\ORM\Mapping as ORM;
-
 trait RolesTrait
 {
-    /**
-     * @ORM\Column(type="json")
-     */
-    private $roles = [];
-    
     public function getRoles(): array
     {
-        $roles = $this->roles;
-        // guarantee every user at least has ROLE_USER
-        $roles[] = self::DEFAULT_ROLE;
-        
-        return array_unique($roles);
+        return $this->roles;
+    }
+    
+    public function setRoles(array $roles)
+    {
+        $this->roles = $roles;
+    }
+    
+    public function addRole(string $newRole)
+    {
+        $this->roles[] = $newRole;
+    }
+
+    public function removeRole(string $oldRole)
+    {
+        $this->roles = array_diff($this->roles, [$oldRole]);
     }
 }
