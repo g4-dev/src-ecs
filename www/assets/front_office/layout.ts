81216291c28c7aecd319f34f7f610e7a8e332754
unknown <julien.boutet.18@gmail.com>
2020-02-01 16:57:10 +0100

    Fix debut layout.ts CI

--- www/assets/front_office/layout.ts
+++ www/assets/front_office/layout.ts
@@ -5,31 +5,42 @@ require('./scss/partials/layout.scss');
     document.getElementById("burger_nav").style.width = "100%"
 }
 */
+
+let burger_nav = document.getElementById("burger_nav");
+let burgerIco = document.getElementById("rotation");
+let nav_children = document.getElementById("a_list");
+
 let isClose = true;
 
 function closeNav() {
-    document.getElementById("burger_nav").style.width = "0%";
-    isClose = true;
+    if(burger_nav !== null){
+        burger_nav.style.width = "0%";
+        isClose = true;
+    }
 }
 
 function rotate() {
-    document.getElementById("rotation").classList.toggle("change")
+    if(burgerIco !== null) {
+        burgerIco.classList.toggle("change")
+    }
 }
 
 function burger() {
-    isClose ? (document.getElementById("burger_nav").style.width = "100%", isClose = false) : (document.getElementById("burger_nav").style.width = "0%", isClose = true)
+    if(burger_nav !== null) {
+        isClose ? (burger_nav.style.width = "100%", isClose = false) : (burger_nav.style.width = "0%", isClose = true)
+    }
 }
 
-let burgerNav = document.getElementById("rotation");
-let nav_children = document.getElementById("a_list");
+if(nav_children !==null){
+    nav_children.addEventListener('click', function () {
+        burger();
+        rotate();
+    });
+}
 
-
-nav_children.addEventListener('click', function () {
-    burger();
-    rotate();
-});
-
-burgerNav.addEventListener('click', function () {
-    closeNav();
-    rotate();
-});
+if(burgerIco !==null) {
+    burgerIco.addEventListener('click', function () {
+        closeNav();
+        rotate();
+    });
+}
