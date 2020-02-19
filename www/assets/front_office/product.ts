d0dd34fbae7b9111cdbde64879f18d42bd324fe1
Valentin Freychet <valentin.freychet@orange.fr>
2020-02-19 15:45:10 +0100

    Anim page product

--- www/assets/front_office/product.ts
+++ www/assets/front_office/product.ts
@@ -4,11 +4,23 @@ const $ = require('jquery');
 let croix = document.getElementsByClassName('croix');
 let hide = document.getElementsByClassName('hide');
 
+let couleur = $('.couleur li');
+let taille = $('.taille li');
+
 for(let i=0; i<3; i++){
     croix[i].addEventListener('click', function(){
         hide[i].classList.toggle('animHide');
     });
 }
+
+couleur.click(function(){
+    $(this).append("<div class='selectCouleur'></div>");
+    $(this).attr('select', 'true');
+});
+
+taille.click(function(){
+   $(this).toggleClass('selectTaille');
+});
 
 
 
