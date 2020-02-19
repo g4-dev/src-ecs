dd355e295a50a3a0bb332f4d266976e3acfb87f8
Valentin Freychet <valentin.freychet@orange.fr>
2020-02-19 14:37:55 +0100

    Page product version desktop OK + modif page DIY

--- www/assets/front_office/product.ts
+++ www/assets/front_office/product.ts
@@ -1,1 +1,15 @@
 require('./scss/product.scss');
+const $ = require('jquery');
+
+let croix = document.getElementsByClassName('croix');
+let hide = document.getElementsByClassName('hide');
+
+for(let i=0; i<3; i++){
+    croix[i].addEventListener('click', function(){
+        hide[i].classList.toggle('animHide');
+    });
+}
+
+
+
+
