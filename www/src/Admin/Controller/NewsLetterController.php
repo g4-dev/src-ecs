51c4eed244580e7728419720435d94516bf0d1ba
Maxime <fabbri.mmaxime@gmail.com>
2020-03-12 14:41:41 +0100

    contoller News letter

---
+++ www/src/Admin/Controller/NewsLetterController.php
@@ -0,0 +1,16 @@
+<?php
+
+
+namespace Admin\Controller;
+
+
+use AlterPHP\EasyAdminExtensionBundle\Controller\EasyAdminController;
+use Core\Service\MailerService;
+
+class NewsLetterController extends EasyAdminController
+{
+    public function newNewsLetterAction(MailerService $mailer)
+    {
+
+    }
+}
