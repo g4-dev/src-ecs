5bc8bafb64d14bd3c195c995c6656bc1da8b7e9a
unknown <julien.boutet.18@gmail.com>
2020-03-12 13:26:11 +0100

    ajout du lien

--- www/src/FrontOffice/Controller/CompanyController.php
+++ www/src/FrontOffice/Controller/CompanyController.php
@@ -6,19 +6,17 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class CompanyController extends AbstractController
 {
+    // TODO : Take this his example to build a well organized page
     /**
-     * @Route("/company", name="company")
+     * @Route("/template-manual", name="companyTemplate")
      */
     public function company()
     {
-        return $this->render('front_office/company.html.twig');
-    }
-
-    /**
-     * @Route("/zerodechet", name="zerodechet")
-     */
-    public function zero()
-    {
-        return $this->render('front_office/zerodechet.html.twig');
+        $content = "never do a thing like that (be generic)";
+        return $this->render('front_office/cms/cmsShow.html.twig',
+           [
+              "content" => $content,
+               "layout" => 'two-cols'
+           ]);
     }
 }
