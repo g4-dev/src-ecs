d3e6f23b5ca2bbd39bcf29ba49d7f18be5aa600c
unknown <julien.boutet.18@gmail.com>
2020-03-12 10:20:24 +0100

    fix + page client

--- www/src/FrontOffice/Controller/CompanyController.php
+++ www/src/FrontOffice/Controller/CompanyController.php
@@ -6,17 +6,19 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class CompanyController extends AbstractController
 {
-    // TODO : Take this his example to build a well organized page
     /**
-     * @Route("/template-manual", name="company")
+     * @Route("/company", name="company")
      */
     public function company()
     {
-        $content = "never do a thing like that (be generic)";
-        return $this->render('front_office/cms/cmsShow.html.twig',
-           [
-              "content" => $content,
-               "layout" => 'two-cols'
-           ]);
+        return $this->render('front_office/company.html.twig');
+    }
+
+    /**
+     * @Route("/zerodechet", name="zerodechet")
+     */
+    public function zero()
+    {
+        return $this->render('front_office/zerodechet.html.twig');
     }
 }
