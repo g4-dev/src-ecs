458adcec7536319cd8f7a63fd78f4c48e7758e9c
Maxime <fabbri.mmaxime@gmail.com>
2020-03-12 14:44:30 +0100

    contoller News letter

--- www/src/Admin/Controller/NewsLetterController.php
+++ www/src/Admin/Controller/NewsLetterController.php
@@ -5,12 +5,22 @@ namespace Admin\Controller;
 
 
 use AlterPHP\EasyAdminExtensionBundle\Controller\EasyAdminController;
+use Core\Entity\User;
 use Core\Service\MailerService;
 
 class NewsLetterController extends EasyAdminController
 {
     public function newNewsLetterAction(MailerService $mailer)
     {
-
+        $user = $this->getDoctrine()->getRepository(User::class)->findBy();
+        try {
+        $mailer->twigSendPurchase(
+            'Purchase Success',
+            $user,
+            'mail/order_confirmation.html.twig',
+            );
+    } catch (\Exception $e) {
+        $this->createNotFoundException();
+        }
     }
 }
